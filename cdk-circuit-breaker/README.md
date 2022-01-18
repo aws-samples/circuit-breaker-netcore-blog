13963bacf722386bb7df8f489f6950c99d82df55
Anitha Deenadayalan <anithade@amazon.com>
2022-01-18 15:24:49 +0800

    CDK for circuit breaker pattern

---
+++ cdk-circuit-breaker/README.md
@@ -0,0 +1,39 @@
+# Implementing the circuit breaker pattern using AWS Step Functions and Amazon DynamoDB
+
+## Prerequisites:
+
+-	An AWS account
+-	An AWS user with AdministratorAccess (see the instructions on the AWS Identity and Access Management (IAM) console)
+-	Access to the following AWS services: AWS Lambda, AWS Step Functions, and Amazon DynamoDB
+-	.NET Core 3.1 SDK installed
+-	JetBrains Rider or Microsoft Visual Studio 2017 or later (or Visual Studio Code)
+
+## Step 1: Download the application
+
+```shell
+$ git clone https://github.com/aws-samples/circuit-breaker-netcore-blog.git
+```
+
+## Step 2: Create packages of lambda functions
+
+The Lambda functions in the circuit-breaker directory must be packaged and copied to the cdk-circuit-breaker\lambdas directory before deployment. Run these commands to process the GetCircuitStatusLambda function: 
+
+```shell
+$ cd circuit-breaker-src/GetCircuitStatusLambda/src/GetCircuitStatusLambda 
+$ dotnet lambda package
+$ cp bin/Release/netcoreapp3.1/GetCircuitStatusLambda.zip ../../../../cdk-circuit-breaker/lambdas
+```
+
+Repeat the same commands for all the Lambda functions in the circuit-breaker-src directory.
+
+## Step 3: Deploy the CDK code
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app. It uses the [.NET Core CLI](https://docs.microsoft.com/dotnet/articles/core/) to compile and execute your project. Build and deploy the CDK code using the commands below.
+
+```shell
+$ npm install -g aws-cdk
+$ cd cdk-circuit-breaker/src/CdkCircuitBreaker && dotnet build
+$ cd ../..
+$ cdk synth
+$ cdk deploy
+```
---
+++ cdk-circuit-breaker/src/CdkCircuitBreaker/CdkCircuitBreakerStack.cs
@@ -0,0 +1,165 @@
+using System.Collections.Generic;
+using Amazon.CDK;
+using Amazon.CDK.AWS.DynamoDB;
+using Amazon.CDK.AWS.IAM;
+using Amazon.CDK.AWS.Lambda;
+using Amazon.CDK.AWS.StepFunctions;
+using Amazon.CDK.AWS.StepFunctions.Tasks;
+
+namespace CdkCircuitBreaker
+{
+    public class CdkCircuitBreakerStack : Stack
+    {
+        internal CdkCircuitBreakerStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            #region iamroles
+            var iamLambdaRole = new Role(this,"LambdaExecutionRole", new RoleProps
+            {
+                RoleName = "LambdaExecutionRole",
+                AssumedBy = new ServicePrincipal("lambda.amazonaws.com")
+            });
+            
+            iamLambdaRole.AddManagedPolicy(ManagedPolicy.FromAwsManagedPolicyName("AmazonDynamoDBFullAccess"));
+            //iamLambdaRole.AddManagedPolicy(ManagedPolicy.FromAwsManagedPolicyName("SecretsManagerReadWrite"));
+            iamLambdaRole.AddManagedPolicy(ManagedPolicy.FromAwsManagedPolicyName("CloudWatchLogsFullAccess"));
+            iamLambdaRole.AddManagedPolicy(ManagedPolicy.FromAwsManagedPolicyName("AWSXrayFullAccess"));
+            //iamLambdaRole.AddManagedPolicy(ManagedPolicy.FromManagedPolicyArn(this,"AWSLambdaVPCAccessExecutionRole","arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"));
+            iamLambdaRole.AddManagedPolicy(ManagedPolicy.FromAwsManagedPolicyName("AWSStepFunctionsFullAccess"));
+
+            var iamStepFunctionRole = new Role(this,"step_functions_basic_execution", new RoleProps
+            {
+                RoleName = "step_functions_basic_execution",
+                AssumedBy = new ServicePrincipal("states.amazonaws.com")
+            });
+            
+            iamStepFunctionRole.AddManagedPolicy(ManagedPolicy.FromAwsManagedPolicyName("CloudWatchLogsFullAccess"));
+            iamStepFunctionRole.AddManagedPolicy(ManagedPolicy.FromManagedPolicyArn(this,"AWSLambdaRole","arn:aws:iam::aws:policy/service-role/AWSLambdaRole"));
+            iamStepFunctionRole.AddManagedPolicy(ManagedPolicy.FromAwsManagedPolicyName("AWSXrayFullAccess"));
+            #endregion iamroles
+            
+            #region DynamoDB tables
+            
+            var circuitBreakerTable = new Table(this, "CircuitBreaker", new TableProps
+            {
+                TableName = "CircuitBreaker",
+                PartitionKey = new Attribute
+                {
+                    Name = "ServiceName",
+                    Type = AttributeType.STRING
+                },
+                TimeToLiveAttribute = "ExpireTimeStamp",
+                RemovalPolicy = RemovalPolicy.DESTROY
+            });
+            
+            #endregion
+            
+            #region Lambda Functions
+
+            var getCircuitStatusLambda = new Function(this,"GetCircuitStatus", new FunctionProps
+            {
+                FunctionName = "GetCircuitStatus",
+                Runtime = Runtime.DOTNET_CORE_3_1,
+                Handler = "GetCircuitStatusLambda::GetCircuitStatusLambda.GetCircuitStatus::FunctionHandler",
+                Role = iamLambdaRole,
+                Code = Code.FromAsset("lambdas/GetCircuitStatusLambda.zip"),
+                Timeout = Duration.Seconds(300)
+            });
+            
+            var updateCircuitStatusLambda = new Function(this,"UpdateCircuitStatus", new FunctionProps
+            {
+                FunctionName = "UpdateCircuitStatus",
+                Runtime = Runtime.DOTNET_CORE_3_1,
+                Handler = "UpdateCircuitStatusLambda::UpdateCircuitStatusLambda.UpdateCircuitStatus::FunctionHandler",
+                Role = iamLambdaRole,
+                Code = Code.FromAsset("lambdas/UpdateCircuitStatusLambda.zip"),
+                Timeout = Duration.Seconds(300)
+            });
+            
+            #endregion
+            
+            #region stepfunction
+             
+            var circuitClosed = new Succeed(this,"Circuit Closed");
+            var circuitOpen = new Fail(this, "Circuit Open");
+            
+            var getCircuitStatusTask = new LambdaInvoke(this, "Get Circuit Status", new LambdaInvokeProps
+            {
+                LambdaFunction = getCircuitStatusLambda,
+                Comment = "Get Circuit Status",
+                RetryOnServiceExceptions = false,
+                PayloadResponseOnly = true
+            });
+            
+            var updateCircuitStatusTask = new LambdaInvoke(this, "Update Circuit Status", new LambdaInvokeProps
+            {
+                LambdaFunction = updateCircuitStatusLambda,
+                Comment = "Update Circuit Status",
+                RetryOnServiceExceptions = false,
+                PayloadResponseOnly = true
+            }).Next(circuitOpen);
+
+            var stateJson = new Dictionary<string, object>
+            {
+                {"Type", "Task"},
+                {"Next", "Circuit Closed"},
+                {"Resource", "arn:aws:states:::lambda:invoke"},
+                {
+                    "Parameters", new Dictionary<string, object>
+                    {
+                        {"FunctionName.$", "$.TargetLambda"}
+                    }
+                },
+                {
+                    "Comment",
+                    "Task to execute lambda. This will set circuit status to OPEN if the execution fails for three times or the task times out"
+                },
+                {"TimeoutSeconds", 12},
+                {
+                    "Retry", new []
+                    {
+                        new Dictionary<string,object>()
+                        {
+                            {"BackoffRate", 1.5},
+                            {"MaxAttempts", 3},
+                            {"IntervalSeconds", 2},
+                            {"ErrorEquals", new string[] {Errors.TASKS_FAILED, Errors.TIMEOUT}}
+                        }
+                    }
+                },
+                {
+                    "Catch", new []
+                    {
+                        new Dictionary<string, object>()
+                        {
+                            {"ErrorEquals", new string[] {Errors.TASKS_FAILED, Errors.TIMEOUT}},
+                            {"Next", "Update Circuit Status"},
+                            {"ResultPath", "$.taskresult"}
+                        }
+                    }
+                }
+            };
+            
+            var executeLambdaTask = new CustomState(this, "Execute Lambda", new CustomStateProps
+            {
+                StateJson = stateJson
+                
+            });
+
+            var stepDefinition = Chain.Start(getCircuitStatusTask)
+                .Next(new Choice(this, "Is Circuit Closed")
+                    .When(Condition.StringEquals("$.CircuitStatus", "OPEN"), circuitOpen)
+                    .When(Condition.StringEquals("$.CircuitStatus", ""), executeLambdaTask.Next(circuitClosed)));
+         
+            var stateMachine = new StateMachine(this, "CircuitBreaker-StepFunction", new StateMachineProps {
+                StateMachineName = "CircuitBreaker-StepFunction",
+                StateMachineType = StateMachineType.STANDARD,
+                Role = iamStepFunctionRole,
+                TracingEnabled = true,
+                Definition = stepDefinition
+            });
+            
+            #endregion
+        }
+    }
+}
+
